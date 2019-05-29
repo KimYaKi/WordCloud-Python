daa73b044c66e9c2730e1ce4c24ad498d23626b3
kimjiha <rlawl@DESKTOP-KOHV3DK>
2019-05-29 17:17:45 +0900

    GET방식 요청 사진 워드클라우드 출력

--- word_cloud.py
+++ word_cloud.py
@@ -10,22 +10,26 @@ import matplotlib.pyplot as plt
 from flask import Flask, request, jsonify
 
 # 플라스크 웹 서버 객체 생성
-app = Flask(__name__)
+app = Flask(__name__, static_folder='outputs')
 
 # 폰트 경로 설정
 font_path = 'NanumGothic.ttf'
 
 def get_tags(text, max_count, min_length):
+    # 명사 추출
     t = Twitter()
     nouns = t.nouns(text)
-    # 단어의 길이가 min_length보다 같거나 클 때만 단어를 저장
-    processed = [n for  n in nouns if len(n) >= min_length]
-    count = Counter(nouns)
+    # 단어의 길이가 min_length 보다 같거나 클 때만 단어를 저장
+    processed = [n for n in nouns if len(n) >= min_length]
+    # 모든 명사의 출현 빈도를 계산
+    count = Counter(processed)
     result = {}
 
-    # 사용 빈도가 높은 (max_count)단어만 사용
+    # 사용 빈도가 높은 max_count 개의 명사 단어만 사용
     for n, c in count.most_common(max_count):
         result[n] = c
+
+    # 추출된 단어가 하나도 없는 경우 "내용이 없습니다"출력
     if len(result) == 0:
         result["내용이 없습니다."] = 1
     return result
@@ -33,9 +37,9 @@ def get_tags(text, max_count, min_length):
 # 만들고자 하는 워드 클라우드의 기본 설정을 진행
 def make_cloud_image(tags, file_name):
     word_cloud = WordCloud(
-        font_path = font_path,
-        width = 800,
-        height = 800,
+        font_path=font_path,
+        width=800,
+        height=800,
         background_color="white"
     )
     # 명사의 빈도수를 출력
@@ -47,13 +51,14 @@ def make_cloud_image(tags, file_name):
     fig.savefig("outputs/{0}.png".format(file_name))
 
 # 단어의 최소 길이와 최대 빈도 수 만큼의 단어만 뽑는다
-def process_from_text(text, max_count, min_length, words):
+def process_from_text(text, max_count, min_length, words, file_name):
     tags = get_tags(text, max_count, min_length)
     # 단어 가중치 적용
     for n, c in words.items():
         if n in tags:
             tags[n] = tags[n] * int(words[n])
-    make_cloud_image(tags, "output")
+    # 명사의 출현 빈도 정보를 통해 워드 클라우드 이미지 생성
+    make_cloud_image(tags, file_name)
 
 @app.route("/process", methods=['GET', 'POST'])
 def process():
@@ -66,10 +71,15 @@ def process():
     # maxCount : 워드클라우드 이미지에 포함 될 최대 단어의 개수
     # minLength : 너무 길이가 짧은 몇몇 단어(단, 즉,)는 제외할 때 사용
     # words : 가중치
-    process_from_text(content['text'], content['maxCount'], content['minLength'], words)
-    result = {'result':True}
+    process_from_text(content['text'], content['maxCount'], content['minLength'], words, content['textID'])
+    result = {'result': True}
     # words라는 변수를 출력하도록(json)
-    return jsonify(words)
+    return jsonify(result)
+
+@app.route('/outputs', methods=['GET', 'POST'])
+def output():
+    text_id = request.args.get('textID')
+    return app.send_static_file(text_id + '.png')
 
 if __name__ == '__main__':
     # 5000 port를 이용하여 구동
